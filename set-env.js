2dc7bbc0f9c736451edb443fa5d61c9bbb6e5f9d
Ayush Gohil <ayush.gohil12@gmail.com>
2026-05-03 17:13:39 +0530

    Update set-env.js

--- set-env.js
+++ set-env.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const env = `export const environment = {
   production: true,
   supabaseUrl: '${process.env.SUPABASE_URL}',
-  supabaseAnonKey: '${process.env.SUPABASE_ANON_KEY}',
+  supabaseKey: '${process.env.SUPABASE_KEY}',
 };
 `;
 
